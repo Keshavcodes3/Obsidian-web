1d5fff665de8c6d0a8554d7c3e86006048a1e7d5
Keshavcodes3 <Keshavcodes0@gmail.com>
2026-08-06 18:59:32 +0530

    feat :adding workspace listener and integrating into app.ts

---
+++ Server/src/common/Listener/workspace.listener.ts
@@ -0,0 +1,15 @@
+import { eventBus } from "@/common/events";
+import { WORKSPACE_EVENTS } from "@/modules/Workspace/events/workspace.event";
+import { Types } from "mongoose";
+
+
+
+eventBus.subscribe(
+    WORKSPACE_EVENTS.CREATED, async (event: {
+        workspaceId:Types.ObjectId,
+        name:string,
+        slug:string
+    }) => {
+        console.log(`Workspace with name ${name} created , welcome now you can edit`)
+    }   
+)
